5a639574066327d231e5e1857d75db91b5510c9f
FabioSimoDev <fabiofabietto182@gmail.com>
2025-02-22 17:05:39 +0100

    Crea url corto
    -Modificato l'hook useFetch, aggiunta l'opzione 'lazy' per inizializzare la richiesta senza senza farla partire direttamente (utile se bisogna aspettare che l'utente inserisca delle informazioni prima di proseguire).
    -Creato hook 'useInput' per gestire i cambiamenti di input.
    -Ora il componente UrlShortener funziona. manda correttamente una richiesta POST al server per generare un url corto.

--- client/src/components/UrlShortener.jsx
+++ client/src/components/UrlShortener.jsx
@@ -1,15 +1,31 @@
+import useFetch from "../hooks/useFetch";
+import useInput from "../hooks/useInput";
+//TODO: trova un modo pe rrendere fluido il cambio di dimensioni del div quando compare il link accorciato
+
 export function UrlShortener() {
+  const { value: url, onChange } = useInput("");
+  const { data, loading, error, refetch } = useFetch(
+    "https://localhost:3000/shorten",
+    { lazy: true, method: "POST" }
+  );
   return (
     <div className="bg-gray-800 p-4 rounded-2xl shadow-xl w-96">
       <p className="text-lg font-semibold text-white">URL Shortener</p>
       <input
         type="text"
         placeholder="https://example.com/your-long-url"
+        onChange={onChange}
+        value={url}
         className="transition-all duration-300 dark:bg-white/50 dark:text-black dark:placeholder-black/50 bg-transparent rounded-full border-0 text-gray-300 placeholder-white/50 w-full p-2"
       />
-      <button className=" mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-lg text-lg">
+      <button
+        className=" mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-lg text-lg"
+        onClick={() => refetch({ body: JSON.stringify({ original_url: url }) })}
+      >
         Accorcia link
       </button>
+      {loading && <p>loading...</p>}
+      {data && !error && <p>link corto: {data.short_url}</p>}
     </div>
   );
 }
