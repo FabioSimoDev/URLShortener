1941a64a1386fcf0e3dacb88fda4522e42c0dbcd
FabioSimoDev <fabiofabietto182@gmail.com>
2025-02-26 10:23:12 +0100

    Custom Hook | SOLID
    -Ristrutturato il codice del componente UrlShortener, iniziava ad essere lungo e a perdere il suo principio di Single Responsibility, quindi ho creato un hook per la transizione dell'altezza e un componente a parte per la TextArea che mostra il link accorciato.

--- client/src/components/UrlShortener.jsx
+++ client/src/components/UrlShortener.jsx
@@ -1,12 +1,12 @@
 import useFetch from "../hooks/useFetch";
 import useInput from "../hooks/useInput";
-import { useRef, useState, useEffect } from "react";
+import useHeight from "../hooks/useHeight";
+import TextArea from "./TextArea";
 //TODO: aggiungi la funzionalità di copia del link accorciato
 //TODO: per ora il link localhost:3000 è hardcoded, cambialo in una variabile d'ambiente
 //TODO: probabilmente è meglio usare un tag <form> per il form, in modo da poter usare il tasto invio per accorciare il link
 //TODO: aggiungi un'animazione al bottone di copia
 //TODO: probabilmente sarebbe meglio creare un componente per il risultato del link accorciato
-//TODO: probabilmente sarebbe meglio creare un hook per l'animazione del div del risultato
 
 export function UrlShortener() {
   const { value: url, onChange } = useInput("");
@@ -14,22 +14,12 @@ export function UrlShortener() {
     "https://localhost:3000/shorten",
     { lazy: true, method: "POST" }
   );
-
-  const [resultHeight, setResultHeight] = useState(0);
-  const resultRef = useRef(null);
+  const { resultRef, resultHeight } = useHeight(data);
 
   const handleSubmit = () => {
     if (url.trim() === "") return;
     refetch({ body: JSON.stringify({ original_url: url }) });
   };
-
-  useEffect(() => {
-    if (resultRef.current) {
-      setResultHeight(resultRef.current.scrollHeight);
-    } else {
-      setResultHeight(0);
-    }
-  }, [data]);
 
   return (
     <div className="bg-gray-800 p-4 rounded-2xl shadow-xl w-96">
@@ -60,19 +50,10 @@ export function UrlShortener() {
         }`}
       >
         {data && !error && (
-          <div ref={resultRef} className="py-4 px-2 bg-gray-700 rounded-lg">
-            <div className="flex items-center">
-              <input
-                type="text"
-                readOnly
-                value={`https://localhost:3000/${data.short_url}`}
-                className="bg-gray-600 text-white rounded-l-lg p-2 w-full"
-              />
-              <button className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-r-lg">
-                Copy
-              </button>
-            </div>
-          </div>
+          <TextArea
+            result={`https://localhost:3000/${data.short_url}`}
+            resultRef={resultRef}
+          />
         )}
       </div>
     </div>
