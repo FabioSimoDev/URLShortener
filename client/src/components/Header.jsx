de4a83a7287bda1e0d5e9e3a23a4d9cb85904314
FabioSimoDev <fabiofabietto182@gmail.com>
2025-02-26 11:20:53 +0100

    Titolo provvisorio
    -Aggiunto possibile titolo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router";
 export function Header() {
   return (
     <header className="absolute top-0 flex justify-between w-full px-8 py-5">
-      <h1 className="text-xl font-bold">Link...</h1>
+      <h1 className="text-xl font-bold">
+        Thats<span className="text-blue-400">Tiny</span>
+      </h1>
       <nav>
         <Link
           to="/about"
